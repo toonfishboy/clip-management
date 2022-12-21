5660907f43d7d0d15c1654db26c7babeaa6fa5e4
ericstampa <eric.stampa@t-online.de>
2022-12-21 21:37:58 +0100

    Added first svelte components

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { publicClient } from "src/helper/server";
+
+
+    let email = "";
+    let password = "";
+
+    async function register(){
+        publicClient.auth.signUp({
+            email
+        })
+    }
+</script>
